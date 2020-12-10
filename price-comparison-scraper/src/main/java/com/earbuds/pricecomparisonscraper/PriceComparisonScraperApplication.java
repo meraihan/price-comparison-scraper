8cebf32868e9defd6acbd184fe0c2c331b21207a
meraihan <sayedmahmudraihan@gmail.com>
2020-12-10 18:05:38 +0600

    initial structure

---
+++ price-comparison-scraper/src/main/java/com/earbuds/pricecomparisonscraper/PriceComparisonScraperApplication.java
@@ -0,0 +1,13 @@
+package com.earbuds.pricecomparisonscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceComparisonScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceComparisonScraperApplication.class, args);
+	}
+
+}
---
+++ price-comparison-scraper/src/test/java/com/earbuds/pricecomparisonscraper/PriceComparisonScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earbuds.pricecomparisonscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceComparisonScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
